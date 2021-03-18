f3274fe2f84fb666f1aea35bc3a07e45d9c55634
bufaonanet <douglas_bufao@yahoo.com.br>
2021-03-18 11:41:18 -0300

    Subindo o projeto

---
+++ AppMvcBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMvcBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+    }
+}
